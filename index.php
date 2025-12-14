0f2e857e87a3bf08514dcf65e2575e08354b3a78
rohitray1 <rohitray1437@gmail.com>
2025-12-14 16:13:54 +0545

    changed page link

--- index.php
+++ index.php
@@ -28,10 +28,10 @@
 
       <ul class="nav-center">
         <li><a href="#">Home</a></li>
-        <li><a href="./shop.html">Shop</a></li>
-        <li><a href="./blog.html">Blog</a></li>
-        <li><a href="./about.html">About Us</a></li>
-        <li><a href="./contact.html">Contact</a></li>
+        <li><a href="/shop">Shop</a></li>
+        <li><a href="/blog">Blog</a></li>
+        <li><a href="/about">About Us</a></li>
+        <li><a href="/contact">Contact</a></li>
       </ul>
 
       <!-- <div class="nav-right">
